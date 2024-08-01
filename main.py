a90eaf5ab4113c706dd6b2aa50239b62952e3729
strandafredde <strandafredde@gmail.com>
2024-08-02 00:45:57 +0200

    Starting to visualize data

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 base_url = "https://www.blocket.se/annonser/norrbotten/fordon/batar/motorbat?cg=1061&f=p&page={page}&q=b%C3%A5tar&r=1&sort=price"
 
@@ -10,29 +13,40 @@ prices = []
 locations = []
 dates = []
 page_nums = ['1']
-
+# =============================================== Get Data ===============================================
+# Get the ads on a single page
 def get_ads_on_page(page_number):
     url = base_url.format(page=page_number)
     page = requests.get(url).text
     soup = BeautifulSoup(page, 'html.parser')
     ads = soup.find_all('div', class_='styled__Wrapper-sc-1kpvi4z-0 iQpUlz')
     
-    page_num = soup.find('div', class_='Pagination__Wrapper-sc-uamu6s-2 dkwcGr').text.strip()
-    page_nums.append(page_num)
     for ad in ads:
         title = ad.find('span', class_='styled__SubjectContainer-sc-1kpvi4z-9 ekmXle').text.strip()
         price = ad.find('div', class_='Price__StyledPrice-sc-1v2maoc-1 lbJRcp').text.strip()
         location = ad.find('p', class_='styled__TopInfoWrapper-sc-1kpvi4z-22 cRXmkf').text.strip()
+        
+        # Check if the location contains the separator and split accordingly
         if '\xa0·\xa0' in location:
             location = location.split('\xa0·\xa0')[1].strip()
+        
         date = ad.find('p', class_='styled__Time-sc-1kpvi4z-18 jMeTyX').text.strip()
 
-        titles.append(title)
-        prices.append(price)
-        locations.append(location)
-        dates.append(date)
-        
-# Loop through the desired number of pages
+        if price > '0':
+            titles.append(title)
+            prices.append(price)
+            locations.append(location)
+            dates.append(date)
+
+    # Check if the pagination element exists
+    pagination_element = soup.find('div', class_='Pagination__Wrapper-sc-uamu6s-2 dkwcGr')
+    if pagination_element:
+        page_num = pagination_element.text.strip()
+        page_nums.append(page_num)
+    else:
+        print(f"Pagination element not found on page {page_number}")
+
+
 def get_last_number_from_list(lst):
     return int(lst[-1][-1])
 
@@ -43,19 +57,48 @@ last_number = get_last_number_from_list(page_nums)
 page_number = 1
 while page_number <= last_number:
     get_ads_on_page(page_number)
-    time.sleep(1)  # Be polite and avoid hitting the server too hard
-    
-    # Update the last number based on the latest state of the list
+    time.sleep(1)  
+
     last_number = get_last_number_from_list(page_nums)
     page_number += 1
 
-print(titles)
-print("")
-print(prices)
-print("")
-print(locations)
-print("")
-print(dates)
-print("")
-print(page_nums)
-print(page_number)
+# print(titles)
+# print("")
+# print(prices)
+# print("")
+# print(locations)
+# print("")
+# print(dates)
+
+# =============================================== Use Data ===============================================
+
+data = pd.DataFrame({
+    'Title': titles,
+    'Price': prices,
+    'Location': locations,
+    'Date': dates
+})
+
+data.to_csv('blocket_motorbat.csv', index=False)
+print("Data saved to blocket_motorbat.csv")
+
+data = pd.read_csv('blocket_motorbat.csv')
+
+data['Price'] = data['Price'].str.replace('kr', '').str.replace(' ', '').astype(float)
+
+# Omvandla årsinformationen till numeriskt format
+data['Date'] = pd.to_numeric(data['Date'], errors='coerce')
+
+
+city_counts = data['Location'].value_counts()
+
+
+# =============================================== Visualizing ===============================================
+
+plt.figure(figsize=(12, 6))
+city_counts.plot(kind='bar')
+plt.title('Antal båtar till salu per stad')
+plt.xlabel('Stad')
+plt.ylabel('Antal båtar')
+plt.xticks(rotation=90)
+plt.show()
